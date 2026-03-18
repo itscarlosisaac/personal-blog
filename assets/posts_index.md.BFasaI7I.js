79c1155529feaa472a60b9a65611cb17b1ca87ac
itscarlosisaac <itscarlosisaac@users.noreply.github.com>
2026-03-18 04:24:08 +0000

    deploy: aa14f768b358c5e494ac62337215a9465d25a696

---
+++ assets/posts_index.md.BFasaI7I.js
@@ -0,0 +1,1 @@
+import{_ as o,a as p}from"./posts_getting-started-with-electronics.md.vwrQtJNJ.js";import"./chunks/framework.JGASAPg-.js";export{o as __pageData,p as default};
--- assets/posts_second-post.md.CqsUhx3b.js
+++
@@ -1,1 +0,0 @@
-import"./chunks/framework.JGASAPg-.js";import{b as o,s as p}from"./posts_getting-started-with-electronics.md.CmexR3EU.js";export{o as __pageData,p as default};
