1ce941617634c411985dec891b4f11a318043ea6
itscarlosisaac <itscarlosisaac@users.noreply.github.com>
2026-03-18 04:12:19 +0000

    deploy: a11152c8f094f3bc9d6ce5b6fd3072ddc9957e84

---
+++ assets/posts_index.md.Cii1eOh-.js
@@ -0,0 +1,1 @@
+import{_ as o,a as p}from"./posts_getting-started-with-electronics.md.S78C6wMa.js";import"./chunks/framework.BnOz76jS.js";export{o as __pageData,p as default};
---
+++ assets/posts_second-post.md.C72Tu-N2.js
@@ -0,0 +1,1 @@
+import"./chunks/framework.BnOz76jS.js";import{b as o,s as p}from"./posts_getting-started-with-electronics.md.S78C6wMa.js";export{o as __pageData,p as default};
